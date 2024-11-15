f93c86562178df65fb33d036cf7117faa7641ead
anlegent <alegent@edenschool.fr>
2024-11-15 13:52:38 +0100

    ex03

--- main.js
+++ main.js
@@ -24,3 +24,12 @@ function areaCalculator(width, height) {
 
 console.log(areaCalculator(1, 3)); // Résultat attendu: 3
 console.log(areaCalculator(1, 0)); // Résultat attendu: 0
+
+// ex03.js
+
+function areaHardcoreCalculator(x1, y1, x2, y2) {
+  return;
+}
+
+console.log(areaHardcoreCalculator(0, 1, 3, 2)); // Résultat attendu: 3
+console.log(areaHardcoreCalculator(1, 68, 2, 87)); // Résultat attendu: 19

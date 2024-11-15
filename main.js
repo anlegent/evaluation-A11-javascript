775812c7eb2969e6a337ab43ce38030b60909345
anlegent <alegent@edenschool.fr>
2024-11-15 15:01:19 +0100

    ex07 3

--- main.js
+++ main.js
@@ -72,16 +72,16 @@ console.log(getReduction(150, 25)); // Résultat attendu: 45
 // ex07.js
 
 function isNationalIDValid(name, age, zip, password) {
-  if ((name.length = 0)) {
-    return true;
-  } else if (age <= 18 || age >= 65) {
-    return true;
-  } else if (zip < 0 || zip >= 99999) {
-    return true;
-  } else if (password == "qwerty" || "azerty" || "1234") {
-    return true;
-  } else {
+  if (name.length == 0) {
     return false;
+  } else if (age <= 18 && age >= 65) {
+    return false;
+  } else if (zip < 0 && zip >= 99999) {
+    return false;
+  } else if (password == "qwerty" || "azerty" || "1234") {
+    return false;
+  } else {
+    return true;
   }
 }
 

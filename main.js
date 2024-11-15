13c8abffa8131d3c463e7aa9ec0f848695ca016e
anlegent <alegent@edenschool.fr>
2024-11-15 13:42:23 +0100

    ex01

--- main.js
+++ main.js
@@ -6,3 +6,12 @@ function multiplyNumber(a, b) {
 
 console.log(multiplyNumber(0, -1)); // Résultat attendu: 0
 console.log(multiplyNumber(-1, -1)); // Résultat attendu: 1
+
+// ex01.js
+
+function greetings(firstname) {
+  return "Bienvenue, " + firstname + " !";
+}
+
+console.log(greetings("")); // Résultat attendu: "Bienvenue, !"
+console.log(greetings("Ben")); // Résultat attendu: "Bienvenue, Ben !"

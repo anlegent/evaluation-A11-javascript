97e8156629ede1acc4676d7d1e2e8cfe2a8bf472
anlegent <alegent@edenschool.fr>
2024-11-15 14:19:15 +0100

    ex06

--- main.js
+++ main.js
@@ -51,3 +51,20 @@ function isEnoughMoney(price, wallet) {
 
 console.log(isEnoughMoney(10, 0)); // Résultat attendu: false
 console.log(isEnoughMoney(5, 25)); // Résultat attendu: true
+
+// ex06.js
+
+function getReduction(price, age) {
+  if (age <= 5) {
+    return (price / 100) * 0;
+  } else if (age <= 18) {
+    return (price / 100) * 50;
+  } else if (age <= 25) {
+    return (price / 100) * 30;
+  } else if (age <= 25) {
+    return (price / 100) * 0;
+  }
+}
+
+console.log(getReduction(10, 5)); // Résultat attendu: 0
+console.log(getReduction(150, 25)); // Résultat attendu: 45

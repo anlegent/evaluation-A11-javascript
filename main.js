d3fecac0485a2f484d829412b7e277623aefdfcd
anlegent <alegent@edenschool.fr>
2024-11-15 13:43:45 +0100

    ex02

--- main.js
+++ main.js
@@ -15,3 +15,12 @@ function greetings(firstname) {
 
 console.log(greetings("")); // Résultat attendu: "Bienvenue, !"
 console.log(greetings("Ben")); // Résultat attendu: "Bienvenue, Ben !"
+
+// ex02.js
+
+function areaCalculator(width, height) {
+  return width * height;
+}
+
+console.log(areaCalculator(1, 3)); // Résultat attendu: 3
+console.log(areaCalculator(1, 0)); // Résultat attendu: 0

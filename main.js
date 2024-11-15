f1d3b509e6e561dcdc530518869a44cae5ec732a
anlegent <alegent@edenschool.fr>
2024-11-15 13:57:54 +0100

    ex04

--- main.js
+++ main.js
@@ -33,3 +33,12 @@ function areaHardcoreCalculator(x1, y1, x2, y2) {
 
 console.log(areaHardcoreCalculator(0, 1, 3, 2)); // Résultat attendu: 3
 console.log(areaHardcoreCalculator(1, 68, 2, 87)); // Résultat attendu: 19
+
+// ex04.js
+
+function isSumEven(a, b) {
+  return a == b % 2;
+}
+
+console.log(isSumEven(1, 3)); // Résultat attendu: true
+console.log(isSumEven(1, 0)); // Résultat attendu: false
